ce673139e8e9711561d5eae55e99b5ea5fe38509
ThomasVervynck <thomas.vervynck.w9612@student.hogent.be>
2019-05-07 16:00:57 +0200

    Update main.tf

--- KeyVault/main.tf
+++ KeyVault/main.tf
@@ -1,4 +1,4 @@
-# In this file we will create a resource group and add a keyvault so we can use these for the next exercice.
+# In this file we will create a resource group and add a keyvault so we can use these for the next exercise.
 /* 
 resource "type" "name" {
   atribute  =   "value"
